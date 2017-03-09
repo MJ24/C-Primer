fcc4d296080ec4ebf0577d6a038ff8e16ffdcd64
MJ24 <1093939518@qq.com>
2017-03-09 22:18:56 +0800

    add headUpf() for String_usasge.h

--- Basic/string_usage.h
+++ Basic/string_usage.h
@@ -7,7 +7,7 @@ class String_usage
 private:
 	static string toUpper(const string &org)
 	{
-		string result = "";
+		string result;
 		for (auto c : org)
 			result += toupper(c);
 		return result;
@@ -17,6 +17,11 @@ private:
 		for (auto &c : org)
 			c = tolower(c);
 	}
+	static void headUp(string &org)
+	{
+		for (decltype(org.size()) i = 0; i < org.size() && !isspace(org[i]); ++i)
+			org[i] = toupper(org[i]);
+	}
 public:
 	static void show_usage() 
 	{
@@ -24,6 +29,8 @@ public:
 		string st2 = toUpper(st1);
 		cout << st2 << endl;
 		toLower(st2);
+		cout << st2 << endl;
+		headUp(st2);
 		cout << st2 << endl;
 	}
 };
