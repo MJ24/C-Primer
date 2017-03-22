1379dc3ca905662c61e0edad9a80775da878fcb4
MJ24 <1093939518@qq.com>
2017-03-23 00:22:02 +0800

    Complete SstreamUsage

--- Basic/sstream_usage.h
+++ Basic/sstream_usage.h
@@ -10,13 +10,15 @@ class SstreamUsage
 public:
 	static void showUsage()
 	{
-		setPersonInfo();
+		std::vector<PersonInfo> persons;
+		setPersonInfo(persons);
+		printPersonInfo(persons);
 	}
 private:
-	static void setPersonInfo()
+	static void setPersonInfo(std::vector<PersonInfo> &persons)
 	{
+		std::cout << "Please input persons info(name phone1 phone2...):\n";
 		std::string line, word;
-		std::vector<PersonInfo> persons;
 		while (getline(std::cin, line))
 		{
 			PersonInfo p;
@@ -29,6 +31,21 @@ private:
 			persons.push_back(p);
 			std::cout << "Finished recording " + p.name + "'s info.\n";
 		}
+	}
+
+	static void printPersonInfo(std::vector<PersonInfo> &persons)
+	{
+		ostringstream oss;
+		for (auto person : persons)
+		{
+			oss << "Name: " << person.name << "\tPhone(s): ";
+			for (auto phone : person.phones)
+			{
+				oss << phone << "\t";
+			}
+			oss << "\n";
+		}
+		std::cout << oss.str();
 	}
 };
 
